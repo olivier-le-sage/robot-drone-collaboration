45409c9d35c3394a1d797f0c466eaa4418fc503e
Olivier Lesage <olivierlesage86@gmail.com>
2019-11-21 23:19:27 +0000

    Updated Ping ultrasonic sensor code for bugfixes

--- Ultrasonic.c
+++ Ultrasonic.c
@@ -1,12 +1,19 @@
-# include <wiringPi.h>
+#include <stdio.h>
+#include <wiringPi.h>
 #include <time.h>
 
-int main(){
-	#define Pin_Trigger 16
-	#define pin_ECHO  18
+#define Pin_Trigger 16
+#define pin_ECHO 18
 
+int main(){
+	double time_taken;
+	double distance;
+	double stop, start;
+
+	printf("Setting up pins");
 	wiringPiSetupPhys();
 
+	printf("wiringPi set up, setting pin modes");
 	pinMode (Pin_Trigger, OUTPUT);
 	pinMode (pin_ECHO, INPUT);
 
@@ -14,27 +21,29 @@ int main(){
    //Writes the value HIGH or LOW (1 or 0) to the given pin which
    // must have been previously set as an output.
 	// send 10us pulse to trigger
-	digitalWrite (Pin_Trigger, 1)
-	delay(0.00001)
+	printf("writing values to pins");
+	digitalWrite (Pin_Trigger, 1);
+	delay(0.00001);
 
-	digitalWrite(Pin_Trigger , 0)
-	start = clock()
+	digitalWrite(Pin_Trigger , 0);
+	start = clock();
 
-	while (digitalRead(pin_ECHO) == 0){
-		start = clock()
+	printf("reading values from pins");
+	if (digitalRead(pin_ECHO) == 0){
+		start = clock();
 	}
 
-	while (digitalRead(pin_ECHO) == 1){
+	if (digitalRead(pin_ECHO) == 1){
 		//stop = clock() - stop
-		stop = clock()
+		stop = clock();
 	}
 
 	//Calculate time taken
 	//double time_taken = ((double t)/ CLOCKS_PER_SEC);
-	double time_taken = stop - start;
+	time_taken = stop - start;
 
 	// Distance travelled in that time multiplied by speed of sound
-	double distance = (time_taken * 34300) / 2;
+	distance = (time_taken * 34300) / 2;
 
 	printf("%s\n", distance);
 
