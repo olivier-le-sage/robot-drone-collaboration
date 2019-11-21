6072b20f639bd1a9c028f401b3db1c716aadf663
olivier-le-sage <olivierlesage86@gmail.com>
2019-11-21 10:53:20 -0500

    moved sharp IR sensor library into src

--- src/Sharp_gp2d12/SharpIR.c
+++ src/Sharp_gp2d12/SharpIR.c
@@ -8,16 +8,16 @@ int main (){
 
 	bool result = false;
 
-	wiringPiSetup();
+	wiringPiSetupPhys();
 	delay(100);
 
-	// prepare to read the pins 
+	// prepare to read the pins
 
 	pinMode (IRPin_1, INPUT);
 	//pinMode (IRPin_2, INPUT);
 	//pinMode (IRPin_3, INPUT);
 
-	
+
 	// int digitalRead(int pin)
 	// it return the value read at the given pin. it will b
 	// HIGH  or LOW (1 or 0) depending on the logic level at the pin.
