2cd8c35e7a79b02de5ae8dfa9625604bdeeebf42
olivier-le-sage <olivierlesage86@gmail.com>
2019-11-26 14:17:42 -0500

    bugfixes C API code

--- src/MPU_6050/MPU_6050.c
+++ src/MPU_6050/MPU_6050.c
@@ -21,98 +21,6 @@
 
 int fd;
 
-// Python bindings
-static PyObject* py_MPU6050_init(PyObject* self)
-{
-
-}
-static PyObject* py_read_raw_data(PyObject* self, PyObject* args)
-{
-	int addr;
-	if (!PyArg_ParseTuple(args, "i", &addr)) return NULL;
-
-	short high_byte, low_byte, value;
-	high_byte = wiringPiI2CReadReg8(fd, addr);
-	low_byte = wiringPiI2CReadReg8(fd, addr+1);
-	value = (high_byte << 8) | low_byte;
-
-	return Py_BuildValue("h", value);
-}
-static PyObject* py_get_ax(PyObject* self)
-{
-	int Ax, Acc_x;
-
-	Acc_x = read_raw_data(ACC_XOUT);
-	Ax = Acc_x / 16384.0; // divide value by sensitivity scale factor
-
-	return Py_BuildValue("i", Ax);
-}
-static PyObject* py_get_ay(PyObject* self)
-{
-	int Ay, Acc_y;
-
-	Acc_y = read_raw_data(ACC_YOUT);
-	Ay = Acc_y / 16384.0; // divide value by sensitivity scale factor
-
-	return Py_BuildValue("i", Ay);
-}
-static PyObject* py_get_az(PyObject* self)
-{
-	int Az, Acc_z;
-
-	Acc_z = read_raw_data(ACC_ZOUT);
-	Az = Acc_z / 16384.0; // divide value by sensitivity scale factor
-
-	return Py_BuildValue("i", Az);
-}
-static PyObject* py_get_gx(PyObject* self)
-{
-	int Gx, Gyro_x;
-
-	Gyro_x = read_raw_data(GYRO_XOUT);
-	Gx = Gyro_x / 131; // divide value by sensitivity scale factor
-
-	return Py_BuildValue("i", Gx);
-}
-static PyObject* py_get_gy(PyObject* self)
-{
-	int Gy, Gyro_y;
-
-	Gyro_y = read_raw_data(GYRO_YOUT);
-	Gy = Gyro_y / 131; // divide value by sensitivity scale factor
-
-	return Py_BuildValue("i", Gy);
-}
-static PyObject* py_get_gz(PyObject* self)
-{
-	int Gz, Gyro_z;
-
-	Gyro_z = read_raw_data(GYRO_ZOUT);
-	Gz = Gyro_z / 131; // divide value by sensitivity scale factor
-
-	return Py_BuildValue("i", Gz);
-}
-
-// Mapping between python and c function names.
-static PyMethodDef MPU_6050_Module_methods[] = {
-    {"MPU6050_init",  py_MPU6050_init,  METH_VARARGS},
-    {"read_raw_data", py_read_raw_data, METH_VARARGS},
-    {"get_ax",        py_get_ax,        METH_VARARGS},
-    {"get_ay",        py_get_ay,        METH_VARARGS},
-    {"get_az",        py_get_az,        METH_VARARGS},
-    {"get_gx",        py_get_gx,        METH_VARARGS},
-    {"get_gy",        py_get_gy,        METH_VARARGS},
-    {"get_gz",        py_get_gz,        METH_VARARGS},
-    {NULL, NULL}
-};
-
-// Python Module initialisation routine.
-void init_MPU_6050(void)
-{
-    // Init module.
-    (void) Py_InitModule("MPU_6050", MPU_6050_Module_methods);
-}
-
 /* Hardware Initialization: Must be run before anything else. */
 void MPU6050_init(){
 
@@ -138,7 +46,7 @@ short read_raw_data(int addr){
 /* accelerometer values in g, gyroscope values in degrees/second */
 
 int get_ax(){
-	int Ax, Acc_x;
+	short Ax, Acc_x;
 
 	Acc_x = read_raw_data(ACC_XOUT);
 	Ax = Acc_x / 16384.0; // divide value by sensitivity scale factor
@@ -147,7 +55,7 @@ int get_ax(){
 }
 
 int get_ay(){
-	int Ay, Acc_y;
+	short Ay, Acc_y;
 
 	Acc_y = read_raw_data(ACC_YOUT);
 	Ay = Acc_y / 16384.0; // divide value by sensitivity scale factor
@@ -156,7 +64,7 @@ int get_ay(){
 }
 
 int get_az(){
-	int Az, Acc_z;
+	short Az, Acc_z;
 
 	Acc_z = read_raw_data(ACC_ZOUT);
 	Az = Acc_z / 16384.0; // divide value by sensitivity scale factor
@@ -165,7 +73,7 @@ int get_az(){
 }
 
 int get_gx(){
-	int Gx, Gyro_x;
+	short Gx, Gyro_x;
 
 	Gyro_x = read_raw_data(GYRO_XOUT);
 	Gx = Gyro_x / 131; // divide value by sensitivity scale factor
@@ -174,7 +82,7 @@ int get_gx(){
 }
 
 int get_gy(){
-	int Gy, Gyro_y;
+	short Gy, Gyro_y;
 
 	Gyro_y = read_raw_data(GYRO_YOUT);
 	Gy = Gyro_y / 131; // divide value by sensitivity scale factor
@@ -183,10 +91,114 @@ int get_gy(){
 }
 
 int get_gz(){
-	int Gz, Gyro_z;
+	short Gz, Gyro_z;
 
 	Gyro_z = read_raw_data(GYRO_ZOUT);
 	Gz = Gyro_z / 131; // divide value by sensitivity scale factor
 
 	return Gz;
+}
+
+// Python bindings
+static PyObject* py_MPU6050_init(PyObject* self)
+{
+		fd = wiringPiI2CSetup(DEVICE_ID);
+
+		wiringPiI2CWriteReg8 (fd, SMPLRT_DIV, 0x07);	/* Write to sample rate register */
+		wiringPiI2CWriteReg8 (fd, PWR_MGMT_1, 0x01);	/* Write to power management register */
+		wiringPiI2CWriteReg8 (fd, CONFIG, 0);			/* Write to Configuration register */
+		wiringPiI2CWriteReg8 (fd, GYRO_CONFIG, 24);		/* Write to Gyro Configuration register */
+		wiringPiI2CWriteReg8 (fd, INT_ENABLE, 0x01);	/*Write to interrupt enable register */
+}
+static PyObject* py_read_raw_data(PyObject* self, PyObject* args)
+{
+	int addr;
+	if (!PyArg_ParseTuple(args, "i", &addr)) return NULL;
+
+	short high_byte, low_byte, value;
+	high_byte = wiringPiI2CReadReg8(fd, addr);
+	low_byte = wiringPiI2CReadReg8(fd, addr+1);
+	value = (high_byte << 8) | low_byte;
+
+	return Py_BuildValue("h", value);
+}
+static PyObject* py_get_ax(PyObject* self)
+{
+	int Ax;
+	short Acc_x;
+
+	Acc_x = read_raw_data(ACC_XOUT);
+	Ax = Acc_x / 16384.0; // divide value by sensitivity scale factor
+
+	return Py_BuildValue("i", Ax);
+}
+static PyObject* py_get_ay(PyObject* self)
+{
+	int Ay
+	short Acc_y;
+
+	Acc_y = read_raw_data(ACC_YOUT);
+	Ay = Acc_y / 16384.0; // divide value by sensitivity scale factor
+
+	return Py_BuildValue("i", Ay);
+}
+static PyObject* py_get_az(PyObject* self)
+{
+	int Az
+	short Acc_z;
+
+	Acc_z = read_raw_data(ACC_ZOUT);
+	Az = Acc_z / 16384.0; // divide value by sensitivity scale factor
+
+	return Py_BuildValue("i", Az);
+}
+static PyObject* py_get_gx(PyObject* self)
+{
+	int Gx
+	short Gyro_x;
+
+	Gyro_x = read_raw_data(GYRO_XOUT);
+	Gx = Gyro_x / 131; // divide value by sensitivity scale factor
+
+	return Py_BuildValue("i", Gx);
+}
+static PyObject* py_get_gy(PyObject* self)
+{
+	int Gy
+	short Gyro_y;
+
+	Gyro_y = read_raw_data(GYRO_YOUT);
+	Gy = Gyro_y / 131; // divide value by sensitivity scale factor
+
+	return Py_BuildValue("i", Gy);
+}
+static PyObject* py_get_gz(PyObject* self)
+{
+	int Gz
+	short Gyro_z;
+
+	Gyro_z = read_raw_data(GYRO_ZOUT);
+	Gz = Gyro_z / 131; // divide value by sensitivity scale factor
+
+	return Py_BuildValue("i", Gz);
+}
+
+// Mapping between python and c function names.
+static PyMethodDef MPU_6050_Module_methods[] = {
+    {"MPU6050_init",  py_MPU6050_init,  METH_VARARGS},
+    {"read_raw_data", py_read_raw_data, METH_VARARGS},
+    {"get_ax",        py_get_ax,        METH_VARARGS},
+    {"get_ay",        py_get_ay,        METH_VARARGS},
+    {"get_az",        py_get_az,        METH_VARARGS},
+    {"get_gx",        py_get_gx,        METH_VARARGS},
+    {"get_gy",        py_get_gy,        METH_VARARGS},
+    {"get_gz",        py_get_gz,        METH_VARARGS},
+    {NULL, NULL}
+}
+
+// Python Module initialisation routine.
+void init_MPU_6050(void)
+{
+    // Init module.
+    (void) Py_InitModule("MPU_6050", MPU_6050_Module_methods);
 }
